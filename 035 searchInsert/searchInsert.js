ba7a993b7c075fefb3df6f80a3e806ddc2d85d3f
alicsu <Alicsu@163.com>
2018-06-07 14:41:41 +0800

    searchInsert

---
+++ 035 searchInsert/searchInsert.js
@@ -0,0 +1,8 @@
+var searchInsert = function (nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+    	if (target <= nums[i]) return i;
+    }
+    return i;
+}
+
+console.log(searchInsert([1,3,5,6],7));
